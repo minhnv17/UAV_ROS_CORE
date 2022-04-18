6a113580bd8156293245243d4eecca83f67bb2c5
minhnv17 <huytranngoc098@gmail.com>
2022-04-18 15:52:48 +0700

    add new package for navigation, offboard mode control uav

---
+++ src/uavlab411/include/uavlab411/off_board.h
@@ -0,0 +1,41 @@
+#include <ros/ros.h>
+#include <mavros_msgs/State.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/CommandBool.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/PoseStamped.h>
+
+
+#define TIMEOUT(msg, timeout) (msg.header.stamp.isZero() || (ros::Time::now() - msg.header.stamp > timeout) )
+
+class OffBoard
+{
+    public:
+        OffBoard();
+
+    private:
+        ros::NodeHandle nh;
+        // Publisher
+        ros::Publisher pub_cmd_vel;
+        ros::Publisher local_pos_pub;
+
+        // Subscriber
+        ros::Subscriber sub_state;
+        ros::Subscriber sub_cmd_vel;
+
+        // Service client
+        ros::ServiceClient srv_arming, srv_set_mode, srv_takeoff;
+
+        // Function handle
+        void handleState(const mavros_msgs::State::ConstPtr& msg);
+        void handleCmdVel(const geometry_msgs::Twist::ConstPtr& msg);
+
+        // Main function
+        void offboardAndArm();
+        void servive();
+
+        // Variable
+        mavros_msgs::State cur_state;
+        geometry_msgs::Twist new_cmd_vel;
+        geometry_msgs::PoseStamped pose;
+};
--- src/uavlab411/src/off_board.cpp
+++ src/uavlab411/src/off_board.cpp
@@ -1,56 +1,60 @@
-/**
- * @file offb_node.cpp
- * @brief Offboard control example node, written with MAVROS version 0.19.x, PX4 Pro Flight
- * Stack and tested in Gazebo SITL
- */
-
-#include <ros/ros.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <mavros_msgs/CommandBool.h>
-#include <mavros_msgs/SetMode.h>
+#include "uavlab411/off_board.h"
 #include <mavros_msgs/State.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/CommandBool.h>
 
-mavros_msgs::State current_state;
-void state_cb(const mavros_msgs::State::ConstPtr& msg){
-    current_state = *msg;
-}
-
-int main(int argc, char **argv)
+OffBoard::OffBoard()
 {
-    ros::init(argc, argv, "offb_node");
-    ros::NodeHandle nh;
+    sub_state = nh.subscribe<mavros_msgs::State>
+            ("mavros/state", 1, &OffBoard::handleState, this);
+    sub_cmd_vel = nh.subscribe<geometry_msgs::Twist>
+            ("/cmd_vel", 20, &OffBoard::handleCmdVel, this);
 
-    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
-            ("mavros/state", 10, state_cb);
-    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
+    local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
             ("mavros/setpoint_position/local", 10);
-    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
+    pub_cmd_vel = nh.advertise<geometry_msgs::Twist>
+            ("mavros/setpoint_velocity/cmd_vel_unstamped", 20);
+
+    srv_arming = nh.serviceClient<mavros_msgs::CommandBool>
             ("mavros/cmd/arming");
-    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
+    srv_takeoff = nh.serviceClient<mavros_msgs::CommandBool>
+            ("mavros/cmd/takeoff");
+    srv_set_mode = nh.serviceClient<mavros_msgs::SetMode>
             ("mavros/set_mode");
 
-    //the setpoint publishing rate MUST be faster than 2Hz
-    ros::Rate rate(20.0);
+    offboardAndArm();
 
-    // wait for FCU connection
-    while(ros::ok() && !current_state.connected){
-        ros::spinOnce();
-        rate.sleep();
+}
+
+void OffBoard::handleState(const mavros_msgs::State::ConstPtr& msg)
+{
+    cur_state = *msg;
+}
+
+void OffBoard::handleCmdVel(const geometry_msgs::Twist::ConstPtr& msg)
+{
+    if(cur_state.armed)
+    {
+        // new_cmd_vel = *msg;
+        pub_cmd_vel.publish(*msg);
     }
+}
 
+void OffBoard::offboardAndArm()
+{
+    ros::Rate r(10);
     geometry_msgs::PoseStamped pose;
     pose.pose.position.x = 0;
     pose.pose.position.y = 0;
     pose.pose.position.z = 2;
-
     //send a few setpoints before starting
     for(int i = 100; ros::ok() && i > 0; --i){
         local_pos_pub.publish(pose);
         ros::spinOnce();
-        rate.sleep();
+        r.sleep();
     }
 
-    mavros_msgs::SetMode offb_set_mode;
+	mavros_msgs::SetMode offb_set_mode;
     offb_set_mode.request.custom_mode = "OFFBOARD";
 
     mavros_msgs::CommandBool arm_cmd;
@@ -59,17 +63,17 @@ int main(int argc, char **argv)
     ros::Time last_request = ros::Time::now();
 
     while(ros::ok()){
-        if( current_state.mode != "OFFBOARD" &&
+        if( cur_state.mode != "OFFBOARD" &&
             (ros::Time::now() - last_request > ros::Duration(5.0))){
-            if( set_mode_client.call(offb_set_mode) &&
+            if( srv_set_mode.call(offb_set_mode) &&
                 offb_set_mode.response.mode_sent){
                 ROS_INFO("Offboard enabled");
             }
             last_request = ros::Time::now();
         } else {
-            if( !current_state.armed &&
+            if( !cur_state.armed &&
                 (ros::Time::now() - last_request > ros::Duration(5.0))){
-                if( arming_client.call(arm_cmd) &&
+                if( srv_arming.call(arm_cmd) &&
                     arm_cmd.response.success){
                     ROS_INFO("Vehicle armed");
                 }
@@ -80,8 +84,16 @@ int main(int argc, char **argv)
         local_pos_pub.publish(pose);
 
         ros::spinOnce();
-        rate.sleep();
+        r.sleep();
     }
+}
 
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "offb_node");
+
+    OffBoard ob;
+
+    ros::spin();
     return 0;
 }
