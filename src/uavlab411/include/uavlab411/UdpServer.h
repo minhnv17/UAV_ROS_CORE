58d8f2ccc2d91a7054d91c7b1e79365b079b394f
kiencate <kiencate@gmail.com>
2022-07-07 16:11:56 +0700

    fix bug in state message

--- src/uavlab411/include/uavlab411/UdpServer.h
+++ src/uavlab411/include/uavlab411/UdpServer.h
@@ -271,7 +271,7 @@ uint8_t _mav_trim_payload(const char *payload, uint8_t length)
 
 int8_t mode_to_int(string mode)
 {
-	for (int8_t i = 0; i < (end(mode_define)-begin(mode_define)); i++)
+	for (int8_t i = 0; i < (end(mode_define) - begin(mode_define)); i++)
 	{
 		if (mode == mode_define[i])
 			return i;
