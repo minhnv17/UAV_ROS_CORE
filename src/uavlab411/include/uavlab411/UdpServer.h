14b049a34d194be275ac106b38ba18ae9a10be0f
kiencate <kiencate@gmail.com>
2022-07-07 16:10:22 +0700

    fix bug in state message

--- src/uavlab411/include/uavlab411/UdpServer.h
+++ src/uavlab411/include/uavlab411/UdpServer.h
@@ -15,6 +15,7 @@
 #include "mavros_msgs/CommandBool.h"
 #include "mavros_msgs/SetMode.h"
 #include <vector>
+#include <array>
 #include "uavlab411/control_robot_msg.h"
 #include "uavlab411/Takeoff.h"
 #include "uavlab411/Navigate.h"
@@ -270,7 +271,7 @@ uint8_t _mav_trim_payload(const char *payload, uint8_t length)
 
 int8_t mode_to_int(string mode)
 {
-	for (int8_t i = 0; i < 4; i++)
+	for (int8_t i = 0; i < (end(mode_define)-begin(mode_define)); i++)
 	{
 		if (mode == mode_define[i])
 			return i;
